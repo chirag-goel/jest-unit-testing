27e3e535296bdc98ddd8659797c83315c28fde65
Chirag Goel <chirag.goel@flipkart.com>
2019-06-17 16:02:50 +0530

    Examples -> React & Snapshot

--- examples/snapshot/clock.test.js
+++ examples/snapshot/clock.test.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Clock from './clock';
 import renderer from 'react-test-renderer';
 
+// This mocks out setTimeout and other timer functions with mock functions.
 jest.useFakeTimers();
 Date.now = jest.fn(() => 1482363367071);
 
